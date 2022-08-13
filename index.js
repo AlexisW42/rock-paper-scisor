e9c5b4838ca89b9e47a8f7c46301a4cd9b7e9c99
Alexis Guerrero <gerreroalexis063@gmail.com>
2022-08-13 13:56:44 -0400

    added css styles to buttons and event listeners

---
+++ index.js
@@ -0,0 +1,9 @@
+const buttonRock = document.querySelector('#rock');
+buttonRock.onclick = () => alert("rock");
+
+const buttonPaper = document.querySelector('#paper');
+buttonPaper.onclick = () => alert("paper");
+
+const buttonScissors = document.querySelector('#scissors');
+buttonScissors.onclick = () => alert("scissors");
+
